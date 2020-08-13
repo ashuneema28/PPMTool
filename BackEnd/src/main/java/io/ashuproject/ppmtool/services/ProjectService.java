676226ab60f8ba8ebb18e9fd986bf061c21b7dbb
ashuneema28 <ashuneema28@gmail.com>
2020-08-13 02:15:03 -0600

    Handling duplicate id validation

--- BackEnd/src/main/java/io/ashuproject/ppmtool/services/ProjectService.java
+++ BackEnd/src/main/java/io/ashuproject/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.ashuproject.ppmtool.services;
 
 import io.ashuproject.ppmtool.domain.Project;
+import io.ashuproject.ppmtool.exceptions.ProjectIdException;
 import io.ashuproject.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch (Exception ex){
+            throw new ProjectIdException("Project Id '"+project.getProjectIdentifier().toUpperCase() + "' alresdy exists");
+        }
     }
 }
