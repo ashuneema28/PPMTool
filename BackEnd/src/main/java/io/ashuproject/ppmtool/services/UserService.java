f4cc9163c80ff005ef6883245797f97e5c19916c
ashuneema28 <ashuneema28@gmail.com>
2020-08-22 01:24:16 -0600

    Added User Entity to rpovide security

---
+++ BackEnd/src/main/java/io/ashuproject/ppmtool/services/UserService.java
@@ -0,0 +1,34 @@
+package io.ashuproject.ppmtool.services;
+
+import io.ashuproject.ppmtool.domain.User;
+import io.ashuproject.ppmtool.exceptions.UsernameAlreadyExistsException;
+import io.ashuproject.ppmtool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser (User newUser){
+        try{
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            //Username has to be unique (exception)
+            newUser.setUsername(newUser.getUsername());
+            // Make sure that password and confirmPassword match
+            // We don't persist or show the confirmPassword
+            newUser.setConfirmPassword("");
+            return userRepository.save(newUser);
+
+        }catch (Exception e){
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+        }
+    }
+
+}
