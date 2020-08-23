8b15c4d2f10fe0ede677926b128d0c03eb869450
ashuneema28 <ashuneema28@gmail.com>
2020-08-23 17:08:56 -0600

    Increaded Token Validity to one day

--- BackEnd/src/main/java/io/ashuproject/ppmtool/security/SecurityConstants.java
+++ BackEnd/src/main/java/io/ashuproject/ppmtool/security/SecurityConstants.java
@@ -6,6 +6,6 @@ public class SecurityConstants {
     public static final String SECRET ="SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX= "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 100_000; //100 seconds
+    public static final long EXPIRATION_TIME = 100000_000; //1 day
 
 }
